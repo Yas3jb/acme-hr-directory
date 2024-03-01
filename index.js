0a22c65a95d47dcfd8210bc5b890d8972d297626
yas3jb <yasmu213@gmail.com>
2024-03-01 16:06:28 -0500

    get method for employees and departments

--- index.js
+++ index.js
@@ -11,15 +11,42 @@ app.use(express.json());
 // Log the requests as they come in
 app.use(require("morgan")("dev"));
 
+// READ DEPARTMENTS - GET
+app.get("/api/departments", async (req, res, next) => {
+  try {
+    const SQL = `
+          SELECT * FROM departments
+          `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+// READ EMPLOYEES - GET
+app.get("/api/employees", async (req, res, next) => {
+  try {
+    const SQL = `
+        SELECT * FROM employees ORDER BY created_at DESC;
+        `;
+    const response = await client.query(SQL);
+    res.send(response.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+
 // Create init function
 const init = async () => {
   await client.connect();
+  console.log("connected to database");
   let SQL = `
         DROP TABLE IF EXISTS employees;
         DROP TABLE IF EXISTS departments;
         CREATE TABLE departments (
             id SERIAL PRIMARY KEY,
-            name VARCHAR(20)
+            name VARCHAR(50)
         );
         CREATE TABLE employees (
             id SERIAL PRIMARY KEY,
@@ -31,18 +58,19 @@ const init = async () => {
         `;
   await client.query(SQL);
   console.log("table created");
-  SQL = `
-        INSERT INTO departments (name) VALUES('Administration');
-        INSERT INTO departments (name) VALUES('Recruitment');
-        INSERT INTO departments (name) VALUES('Wellbeing');
-        INSERT INTO employees (name, department_id) VALUES('Edwin', (SELECT id FROM departments WHERE name='Administration'));
-        INSERT INTO employees (name, department_id) VALUES('Kavin', (SELECT id FROM departments WHERE name='Recruitment'));
-        INSERT INTO employees (name, department_id) VALUES('Liz', (SELECT id FROM departments WHERE name='Wellbeing'));
-        INSERT INTO employees (name, department_id) VALUES('Yasin', (SELECT id FROM departments WHERE name='Recruitment'));
-        INSERT INTO employees (name, department_id) VALUES('John', (SELECT id FROM departments WHERE name='Wellbeing'));
-        INSERT INTO employees (name, department_id) VALUES('Mike', (SELECT id FROM departments WHERE name='Wellbeing'));
-        `;
 
+  SQL = `
+        INSERT INTO departments(name) VALUES('Administration');
+        INSERT INTO departments(name) VALUES('Recruitment');
+        INSERT INTO departments(name) VALUES('Wellbeing');
+        INSERT INTO employees(name, department_id) VALUES('Edwin', (SELECT id FROM departments WHERE name='Administration'));
+        INSERT INTO employees(name, department_id) VALUES('Kavin', (SELECT id FROM departments WHERE name='Recruitment'));
+        INSERT INTO employees(name, department_id) VALUES('Liz', (SELECT id FROM departments WHERE name='Wellbeing'));
+        INSERT INTO employees(name, department_id) VALUES('Yasin', (SELECT id FROM departments WHERE name='Recruitment'));
+        INSERT INTO employees(name, department_id) VALUES('John', (SELECT id FROM departments WHERE name='Wellbeing'));
+        INSERT INTO employees(name, department_id) VALUES('Mike', (SELECT id FROM departments WHERE name='Wellbeing'));
+        `;
+  await client.query(SQL);
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => console.log(`listening on port ${PORT}`));
 };
